a6b5eef242f39a3f090b87750792bfe2282e59b1
helinozlemm <helinozlemm96@gmail.com>
2021-05-18 20:09:39 +0300

    JavaScript and SaaS changed

--- 02-Base-Apparel-Coming-Soon-master/script.js
+++ 02-Base-Apparel-Coming-Soon-master/script.js
@@ -1,1 +1,19 @@
 const button = document.getElementById("send");
+const form = document.getElementById("form");
+const errMessage = document.querySelector(".errmessage");
+const errIcon = document.querySelector(".error_icon");
+
+button.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const email = form["email"].value;
+  const mailFormat =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!email == "" && mailFormat.test(String(email).toLowerCase())) {
+    return true;
+  } else {
+    errMessage.classList.toggle("visible");
+    errIcon.classList.toggle("visible");
+  }
+});
